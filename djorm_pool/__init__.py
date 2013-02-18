4281cc5d6d14ae26d48786451b41f02373b048f9
Andrey Antukh <niwi@niwi.be>
2013-02-18 16:18:44 +0100

    Improved monky patching (avoiding double monky patching).

--- djorm_pool/__init__.py
+++ djorm_pool/__init__.py
@@ -6,7 +6,6 @@ from django.conf import settings
 import logging
 from functools import partial
 
-# Logging
 log = logging.getLogger('djorm.pool')
 _log = lambda msg, *args: log.debug(msg)
 
@@ -15,59 +14,72 @@ if settings.DEBUG:
     event.listen(QueuePool, 'checkin', partial(_log, 'returned to pool'))
     event.listen(QueuePool, 'connect', partial(_log, 'new connection'))
 
-
 POOL_SETTINGS = getattr(settings, 'DJORM_POOL_OPTIONS', {})
 
+def patch_mysql():
+    class hashabledict(dict):
+        def __hash__(self):
+            return hash(tuple(sorted(self.items())))
 
-class hashabledict(dict):
-    def __hash__(self):
-        return hash(tuple(sorted(self.items())))
+    class hashablelist(list):
+        def __hash__(self):
+            return hash(tuple(sorted(self)))
 
+    class ManagerProxy(object):
+        def __init__(self, manager):
+            self.manager = manager
 
-class hashablelist(list):
-    def __hash__(self):
-        return hash(tuple(sorted(self)))
+        def __getattr__(self, key):
+            return getattr(self.manager, key)
 
+        def connect(self, *args, **kwargs):
+            if 'conv' in kwargs:
+                conv = kwargs['conv']
+                if isinstance(conv, dict):
+                    items = []
+                    for k, v in conv.items():
+                        if isinstance(v, list):
+                            v = hashablelist(v)
+                        items.append((k, v))
+                    kwargs['conv'] = hashabledict(items)
+            return self.manager.connect(*args, **kwargs)
 
-class ManagerProxy(object):
-    def __init__(self, manager):
-        self.manager = manager
+    try:
+        from django.db.backends.mysql import base as mysql_base
+    except ImproperlyConfigured:
+        return
 
-    def __getattr__(self, key):
-        return getattr(self.manager, key)
-
-    def connect(self, *args, **kwargs):
-        if 'conv' in kwargs:
-            conv = kwargs['conv']
-            if isinstance(conv, dict):
-                items = []
-                for k, v in conv.items():
-                    if isinstance(v, list):
-                        v = hashablelist(v)
-                    items.append((k, v))
-                kwargs['conv'] = hashabledict(items)
-        return self.manager.connect(*args, **kwargs)
-
-try:
-    from django.db.backends.postgresql_psycopg2 import base as pgsql_base
-    pgsql_base._Database = pgsql_base.Database
-    pgsql_base.Database = manage(pgsql_base._Database, **POOL_SETTINGS)
-except ImproperlyConfigured:
-    pass
-
-
-try:
-    from django.db.backends.mysql import base as mysql_base
-    mysql_base._Database = mysql_base.Database
-    db = ManagerProxy(manage(mysql_base._Database, **POOL_SETTINGS))
-    mysql_base.Database = db
-except ImproperlyConfigured:
-    pass
+    if not hasattr(mysql_base, "_Database"):
+        mysql_base._Database = mysql_base.Database
+        mysql_base.Database = ManagerProxy(manage(mysql_base._Database, **POOL_SETTINGS))
 
 
-try:
-    from django.db.backends.sqlite3 import base as sqlite3_base
-    sqlite3_base._Database = sqlite3_base.Database
-    sqlite3_base.Database = manage(sqlite3_base._Database, **POOL_SETTINGS)
-except ImproperlyConfigured:
-    pass
+def patch_postgresql():
+    try:
+        from django.db.backends.postgresql_psycopg2 import base as pgsql_base
+    except ImproperlyConfigured:
+        return
+
+    if not hasattr(pgsql_base, "_Database"):
+        pgsql_base._Database = pgsql_base.Database
+        pgsql_base.Database = manage(pgsql_base._Database, **POOL_SETTINGS)
+
+
+def patch_sqlite3():
+    try:
+        from django.db.backends.sqlite3 import base as sqlite3_base
+    except ImproperlyConfigured:
+        return
+
+    if not hasattr(sqlite3_base, "_Database"):
+        sqlite3_base._Database = sqlite3_base.Database
+        sqlite3_base.Database = manage(sqlite3_base._Database, **POOL_SETTINGS)
+
+
+def patch_all():
+    patch_mysql()
+    patch_postgresql()
+    patch_sqlite3()
+
+
+patch_all()
